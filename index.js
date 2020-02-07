48d28b2dc6720ed4ad80fad6d552da11367e1c7d
Jos Bogan <Josbogan@gmail.com>
2020-02-07 13:56:07 +0000

    config

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const { port, dbURI } = require('./config/environment')
+
+const app = express()
+
+// ! Connecting to the MongoDB
+mongoose.connect(
+  dbURI, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) return console.log(err)
+    console.log('Mongo is connected!')
+  }
+)
+
+
+
+app.use(bodyParser.json())
+
+app.listen(port, () => console.log(`Port ${port} is up and running`))
