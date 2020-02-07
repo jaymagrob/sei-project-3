5186e4b8cc2d99caae7afa0b147b7d4b1b08450f
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-02-07 14:14:56 +0000

    router

---
+++ config/router.js
@@ -0,0 +1,33 @@
+const router = require('express').Router()
+const projects = require('../controllers/projects')
+const users = require('../controllers/auth')
+// const secureRoute = require('../lib/secureRoute')
+
+router.route('/projects')
+  .get(projects.index)
+  .post(projects.create)
+
+router.route('/projects/:id')
+  .get(projects.show)
+  .put(projects.update)
+  .delete(projects.destroy)
+
+router.route('/projects/:id/comments')
+  .post(projects.commentCreate)
+
+router.route('/projects/:id/comments/:commentId')
+  .delete(projects.commentDelete)
+
+router.route('/projects/:id/like')
+  .get(projects.like)
+
+router.route('/register')
+  .post(users.register)
+
+router.route('/login')
+  .post(users.login)
+
+router.route('/profile')
+  .get(users.profile)
+
+module.exports = router
