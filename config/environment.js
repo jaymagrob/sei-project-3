e0dce9337df4f1554c7c58c532d767cc13e1cd63
Jos Bogan <Josbogan@gmail.com>
2020-02-07 14:52:37 +0000

    user model added

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,5 @@
 const port = 4000
 const dbURI = 'mongodb://localhost/beehive'
+const skills = ['Illustration', '3d Modelling', 'Graphic Design', 'Photography']
 
-module.exports = { port, dbURI }
+module.exports = { port, dbURI, skills }
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const { skills } = require('../config/environment')
+
+const likeSchema = mongoose.Schema({
+  user: { type: mongoose.Schema.ObjectId, ref: 'User', required: true }
+}, {
+  timestamps: true
+})
+
+const userSchema = mongoose.Schema({
+  usernameName: { type: String, require: true, unique: true },
+  name: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  password: { type: String, require: true },
+  bio: { type: String, maxlength: 350 },
+  profileImage: { type: String },
+  location: { type: String },
+  level: { type: String, enam: ['Junior', 'Mid-Level', 'Senior'] },
+  skills: { type: String, enam: skills },
+  projects: [{ type: mongoose.Schema.ObjectId, ref: 'Destination' }],
+  likes: [likeSchema]
+})
+
+// ! deleting password on user when sent to JSON
+userSchema
+  .set('toJSON', {
+    virtuals: true, 
+    transform(doc, json) {
+      delete json.password
+      return json
+    }
+  })
+
+// ! Setting the password Confirmation virtual field 
+userSchema
+  .virtual('passwordConfirmaion') 
+  .set(function setPasswordConfirmaion(passwordConfirmaion) {
+    this._passwordConfirmation = passwordConfirmaion
+  })
+
+userSchema
+  .pre('validate', function checkPasswordMatch(next) {
+    if (this.isModified('password') && this._passwordConfirmation !== this.password) {
+      this.invalidate('PasswordConfirmation', 'does not match')
+    }
+    next()
+  })
+
+module.exports = mongoose.model('User', userSchema)
