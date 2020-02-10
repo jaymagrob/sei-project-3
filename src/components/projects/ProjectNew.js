06a45e332d1bc0c055f878205bb8bba878458960
Jos Bogan <Josbogan@gmail.com>
2020-02-10 11:37:58 +0000

    newproject page

--- src/components/projects/ProjectNew.js
+++ src/components/projects/ProjectNew.js
@@ -1,29 +1,31 @@
 import React from 'react'
 
-class ProjectNew extends React.Component{
+class ProjectNew extends React.Component {
 
-  state = {
-    data: {
-      name: '',
-      owner: '',
-      collaborators: [],
-      description: '',
-      location: '',
-      images: [],
-      completed: false,
-      recuiting: false,
-      skillsInvolved: [],
-      lookingFor: []
-    }
-  }
+  // state = {
+  //   data: {
+  //     name: '',
+  //     owner: '',
+  //     collaborators: [],
+  //     description: '',
+  //     location: '',
+  //     images: [],
+  //     completed: false,
+  //     recuiting: false,
+  //     skillsInvolved: [],
+  //     lookingFor: []
+  //   }
+  // }
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value })
-  }
+  // handleChange = (e) => {
+  //   this.setState({ [e.target.name]: e.target.value })
+  // }
 
   render() {
+    console.log('this is now rendering')
     return (
       <section>
+        gfdsafgnhmfdseadwefsdgfh
         <h1>New Project</h1>
         <form>
           <div>
