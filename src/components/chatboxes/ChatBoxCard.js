a4e5446630471fbe4f26040522a83ff1575cbf3d
Nicolas-Dolan <nicolas.dolan@ymail.com>
2020-02-14 11:01:55 +0000

    message fix

--- src/components/chatboxes/ChatBoxCard.js
+++ src/components/chatboxes/ChatBoxCard.js
@@ -2,15 +2,16 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 // import Auth from '../../lib/auth'
 
-const ChatBoxCard = ({ _id, owner, updatedAt }) => (
+const ChatBoxCard = ({ _id, owner, updatedAt, messages }) => (
   <div key={_id}>
-    <Link to={`users/${owner}/chatboxes/${_id}`}>
+    {messages.length > 0 ? <Link to={`users/${owner}/chatboxes/${_id}`}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Human-emblem-mail-yellow-128.png/120px-Human-emblem-mail-yellow-128.png" alt="message" />
-      {/* <h1>Message</h1> */}
-      {/* <h1>{members[0]} and {members[1]}</h1>
-      <p>Owner: {owner}</p> */}
       <p>{Date(updatedAt).slice(4, 10)}</p>
-    </Link>
+    </Link> : '' }
+    {/* <Link to={`users/${owner}/chatboxes/${_id}`}>
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Human-emblem-mail-yellow-128.png/120px-Human-emblem-mail-yellow-128.png" alt="message" />
+      <p>{Date(updatedAt).slice(4, 10)}</p>
+    </Link> */}
   </div>
 )
 
